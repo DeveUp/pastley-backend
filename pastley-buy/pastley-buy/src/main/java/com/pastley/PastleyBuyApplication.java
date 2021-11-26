144b1acff8c366656cfd184e0c0d4b65419c2d9e
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-26 07:53:27 -0500

    Create buy microservice

---
+++ pastley-buy/pastley-buy/src/main/java/com/pastley/PastleyBuyApplication.java
@@ -0,0 +1,13 @@
+package com.pastley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastleyBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastleyBuyApplication.class, args);
+	}
+
+}
