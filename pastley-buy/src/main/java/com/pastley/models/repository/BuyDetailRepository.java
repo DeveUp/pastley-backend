eaab4d024fc0f4924acd0dbaefc3b034571b761a
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-27 11:26:23 -0500

    Add exception product microservice

--- pastley-buy/src/main/java/com/pastley/models/repository/BuyDetailRepository.java
+++ pastley-buy/src/main/java/com/pastley/models/repository/BuyDetailRepository.java
@@ -1,6 +1,10 @@
 package com.pastley.models.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.pastley.models.entity.BuyDetail;
@@ -8,4 +12,6 @@ import com.pastley.models.entity.BuyDetail;
 @Repository
 public interface BuyDetailRepository extends JpaRepository<BuyDetail, Long> {
 
+	@Query(nativeQuery = false, value = "SELECT bd FROM BuyDetail bd WHERE bd.buy.id= :idBuy")
+	public List<BuyDetail> findByBuy(@Param("idBuy") Long idBuy);
 }
