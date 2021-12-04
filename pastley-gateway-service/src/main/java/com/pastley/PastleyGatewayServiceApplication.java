d9bb4c80ed4e63687ab436caace4488de0798a8c
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-12-04 14:08:09 -0500

    updating variable yml

--- pastley-gateway-service/src/main/java/com/pastley/PastleyGatewayServiceApplication.java
+++ pastley-gateway-service/src/main/java/com/pastley/PastleyGatewayServiceApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @project Pastley-Sale.
+ * @project Pastley-Gateway.
  * @author Sergio Stives Barrios Buitrago.
  * @Github https://github.com/SerBuitrago.
- * @contributors soleimygomez, leynerjoseoa, jhonatanbeltran.
+ * @contributors leynerjoseoa.
  * @version 1.0.0.
  */
 @SpringBootApplication
